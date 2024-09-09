016ef051d60f3f1bb81beb3a34687b617123196c
Saqib <saiyad350078@gmail.com>
2024-09-09 22:08:51 +0530

    Initial commit

---
+++ Happybirthdayapp/app/src/main/java/com/example/happybirthdayapp/MainActivity.kt
@@ -0,0 +1,96 @@
+package com.example.happybirthdayapp
+
+import android.os.Bundle
+import android.os.Message
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalOf
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.happybirthdayapp.ui.theme.HappyBirthdayAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HappyBirthdayAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background,
+                ) {
+                    GreetingImage(message = stringResource(R.string.happy_birthday_text), from = stringResource(
+                        R.string.signature_text
+                    ), modifier = Modifier.padding(18.dp))
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun GreetingText(message: String, modifier: Modifier = Modifier,from: String) {
+    Column(verticalArrangement = Arrangement.Center, modifier = modifier){
+            Text(
+                text = message,
+                fontSize = 70.sp,
+                lineHeight = 116.sp,
+                color = Color.Red,
+                textAlign = TextAlign.Center
+            )
+            Text(
+                text = from,
+                fontSize = 36.sp,
+                color = Color.Cyan,
+                modifier = Modifier
+                    .padding(16.dp)
+                    .align(alignment = Alignment.CenterHorizontally)
+            )
+}
+}
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier= Modifier) {
+    val image = painterResource(R.drawable.androidparty__1_)
+    Box {
+        Image(
+        painter = image,
+        contentDescription = null,
+        contentScale = ContentScale.Crop,
+            alpha = 0.5F
+    )
+        GreetingText(message = message, from = from, modifier= Modifier
+            .fillMaxSize()
+            .padding(16.dp))
+}
+}
+
+@Preview(showBackground = true,
+    showSystemUi = true)
+@Composable
+    fun BirthdayCardPreview() {
+    HappyBirthdayAppTheme {
+        GreetingImage(message = "Happy birthday Sam!", from = "from Emma")
+    }
+}
